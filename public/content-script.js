1668c037e9ffe34f3d2329a51350c828686632d9
laktam abdellah <laktamlaktamabdellah@gmail.com>
2023-07-23 23:49:03 +0100

    add note and shortcut

--- public/content-script.js
+++ public/content-script.js
@@ -4,26 +4,39 @@
 //if there is no notes create an empty one
 
 //2
-// Create the outer container
-var OuterDiv = document.createElement("div");
-OuterDiv.id = "OuterDiv";
-OuterDiv.style.width = "200px";
-OuterDiv.style.height = "400px";
-OuterDiv.style.backgroundColor = "lightblue";
-OuterDiv.style.position = "absolute";
-// document.body.appendChild(mydiv);
 
-// Create the header div with id="mydivheader" inside the mydiv container
-var mydivheader = document.createElement("div");
-mydivheader.id = "mydivheader";
-mydivheader.innerHTML = "Click here to move";
-mydivheader.style.padding = "10px";
-mydivheader.style.cursor = "move";
-mydivheader.style.backgroundColor = "blue";
-mydivheader.style.color = "white";
-mydiv.appendChild(mydivheader);
+document.addEventListener("keydown", function (event) {
+  // Check for Ctrl + Shift + n
+  if (event.ctrlKey && event.shiftKey && event.key === "n") {
+    DraggableDiv = createDraggableDiv();
+    document.appendChild(DraggableDiv);
+  }
+});
 
-// Create the content div with id="content" inside the mydiv container
-var content = document.createElement("div");
-content.id = "content";
-mydiv.appendChild(content);
+function createDraggableDiv() {
+  // Create the outer container
+  var OuterDiv = document.createElement("div");
+  OuterDiv.id = "OuterDiv";
+  OuterDiv.style.width = "200px";
+  OuterDiv.style.height = "400px";
+  OuterDiv.style.backgroundColor = "lightblue";
+  OuterDiv.style.position = "absolute";
+  // document.body.appendChild(mydiv);
+
+  // Create the header div with id="DivHeader" inside the mydiv container
+  var DivHeader = document.createElement("div");
+  DivHeader.id = "DivHeader";
+  DivHeader.innerHTML = "Click here to move";
+  DivHeader.style.padding = "10px";
+  DivHeader.style.cursor = "move";
+  DivHeader.style.backgroundColor = "blue";
+  DivHeader.style.color = "white";
+  OuterDiv.appendChild(DivHeader);
+
+  // Create the content div with id="content" inside the mydiv container
+  var Content = document.createElement("div");
+  Content.id = "content";
+  OuterDiv.appendChild(Content);
+
+  return OuterDiv;
+}
